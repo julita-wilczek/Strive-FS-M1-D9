3b159e95a4e2bd7c487e52c0c2b7b8a1eb30925f
julita-wilczek <julita.wilczek@gmail.com>
2021-12-09 20:21:18 +0100

    Added function to generate Bingo Board

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* TODO IN JS:
-- Generate 76 cells with cells numbered from 1 to 76 (Bingo Board)
-- Add the created bingo board to appropriate div in the html
+- 
 - Add a function for the button to create a randomized number from 1 to 76
 - Randomized number should not be repeated
 - Once the number is generated the cell with the same number should be highlighted on the bingo board
@@ -8,8 +7,25 @@
 - Add a function for the button to create as a user board
 - Add a function to allows creation of as many user boards as selected by the user
 
-*/
+*/ 
 
-function createBingo() {
+//Generate 76 cells with cells numbered from 1 to 76 (Bingo Board)
+// Add the created bingo board to appropriate div in the html
 
+window.onload = function() {
+    createBingoBoard()
 }
+
+function createBingoBoard() {
+
+    let bingoBoard = document.getElementById("bingo-board")
+
+    for (let bingoNumber = 1; bingoNumber <=76; bingoNumber++){
+        let bingoBoardCell = document.createElement("div");
+        bingoBoardCell.innerText = bingoNumber;
+        bingoBoardCell.classList.add("number");
+        bingoBoard.appendChild(bingoBoardCell);
+        
+    }
+}
+
