6c1a95e5a6e1d8b0abf8fb95ac8a2520a1998ff6
julita-wilczek <julita.wilczek@gmail.com>
2021-12-09 19:56:37 +0100

    Added tasks for java script, css

---
+++ index.js
@@ -0,0 +1,15 @@
+/* TODO IN JS:
+- Generate 76 cells with cells numbered from 1 to 76 (Bingo Board)
+- Add the created bingo board to appropriate div in the html
+- Add a function for the button to create a randomized number from 1 to 76
+- Randomized number should not be repeated
+- Once the number is generated the cell with the same number should be highlighted on the bingo board
+- Generate a user board with 24 randomized (and not repeated) numbers that that highlights as the main board does.
+- Add a function for the button to create as a user board
+- Add a function to allows creation of as many user boards as selected by the user
+
+*/
+
+function createBingo() {
+
+}
